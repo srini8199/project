08aa9359a3c5c8a56344925fb000e9d2a05a78ef
Srinivas M <srini8199@gmail.com>
2022-07-31 18:31:40 +0530

    added new configuration 2

--- utils/config.js
+++ utils/config.js
@@ -3,3 +3,4 @@ exports.secretkey = 'mysupersecretpassword';
 exports.dbUrl = 'mongodb://localhost:27017';
 exports.apiKey = 'BJIPIDuWMR3qK6lzQgYQmjNq7eh8QpR6BofUeYIDGBEx';
 exports.apiKey = 'prep-auther';
+exports.apiKey = 'prep-project';
