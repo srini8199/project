4e6ed1b72deeca573c032cce7d4b2970beeb3f43
Srinivas M <srini8199@gmail.com>
2022-07-31 18:48:41 +0530

    added new configuration 3

--- utils/config.js
+++ utils/config.js
@@ -4,3 +4,4 @@ exports.dbUrl = 'mongodb://localhost:27017';
 exports.apiKey = 'BJIPIDuWMR3qK6lzQgYQmjNq7eh8QpR6BofUeYIDGBEx';
 exports.apiKey = 'prep-auther';
 exports.apiKey = 'prep-project';
+exports.apiKey = 'prep-update';
