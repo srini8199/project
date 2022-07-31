ea5fe10f556cd3b5ffb429a567e601ddad0039c9
Srinivas M <srini8199@gmail.com>
2022-07-31 18:22:20 +0530

    added new configuration

--- utils/config.js
+++ utils/config.js
@@ -2,3 +2,4 @@ exports.authTokenExpirationTime = 3600;
 exports.secretkey = 'mysupersecretpassword';
 exports.dbUrl = 'mongodb://localhost:27017';
 exports.apiKey = 'BJIPIDuWMR3qK6lzQgYQmjNq7eh8QpR6BofUeYIDGBEx';
+exports.apiKey = 'prep-auther';
